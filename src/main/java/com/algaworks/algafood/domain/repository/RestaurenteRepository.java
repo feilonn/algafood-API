32cd01486f96993049f36571697d6d04d557c015
Raul Correia <raulvinicius245@gmail.com>
2023-05-29 22:44:48 -0300

    feat: Criado repositorios para Cozinha e Restaurante.

---
+++ src/main/java/com/algaworks/algafood/domain/repository/RestaurenteRepository.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurenteRepository extends JpaRepository<Restaurante, Long> {
+
+}
