e081ed8cdd2149f51966cf9b5d5365a327b3aaa6
Raul Correia <raulvinicius245@gmail.com>
2023-05-29 21:02:40 -0300

    feat: Adicionado dialeto INNO DB.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO cozinha(nome) VALUES ('Japonesa');
 INSERT INTO cozinha(nome) VALUES ('Havaiana');
 INSERT INTO cozinha(nome) VALUES ('Mineira');
+
+INSERT INTO restaurante(nome, taxa_frete) VALUES ('Gusteaus', 14.99);
+INSERT INTO restaurante(nome, taxa_frete) VALUES ('Taberna', 14.99)
