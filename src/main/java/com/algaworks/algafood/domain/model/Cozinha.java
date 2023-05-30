de7708ad2980c15a81f5b8922652a65988df4db1
Raul Correia <raulvinicius245@gmail.com>
2023-05-29 22:46:48 -0300

    perf: removido imports inutilizados.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,8 +2,6 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
