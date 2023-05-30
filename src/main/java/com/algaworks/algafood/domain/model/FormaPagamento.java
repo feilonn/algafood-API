65cb6c583fe13a8eb1e58803da41aeccaacf71b9
Raul Correia <raulvinicius245@gmail.com>
2023-05-29 22:43:59 -0300

    feat: Criado entidades e repositorios para Forma de Pagamento e Permissão.

---
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class FormaPagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Permissao.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permissao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+}
