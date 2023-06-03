dc1b5b7bebfa71b15a043fea1ff1ad12175e4447
Raul Correia <raulvinicius245@gmail.com>
2023-06-02 23:02:35 -0300

    feat: Criado exception para Data Integrity Violation.

---
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeSemAtributosObrigatoriosException.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.domain.exception;
+
+public class EntidadeSemAtributosObrigatoriosException extends RuntimeException{
+    public EntidadeSemAtributosObrigatoriosException(String mensagem) {
+        super(mensagem);
+    }
+
+}
