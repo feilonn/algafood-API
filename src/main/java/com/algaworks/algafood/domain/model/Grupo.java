987dbf9ef22d3731f15ced5675ed702802354d46
Raul Correia <raulvinicius245@gmail.com>
2023-06-21 15:51:08 -0300

    feat: Adicionado relacionamento entre Grupo e Permissao.

--- src/main/java/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -14,12 +15,14 @@ public class Grupo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(nullable = false)
     private String nome;
 
     @ManyToMany
+    @JsonIgnore
     @JoinTable(name = "grupo_permissao",
             joinColumns = @JoinColumn(name = "grupo_id"),
             inverseJoinColumns = @JoinColumn(name = "permissao_id"))
